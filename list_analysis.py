ef094ef1d39129217e9db6ca3c9a050bfc7677b4
Razi Falah <92949627+RaziFalah@users.noreply.github.com>
2022-01-04 15:59:13 +0200

    Update list_analysis.py

--- list_analysis.py
+++ list_analysis.py
@@ -1,3 +1,9 @@
+#Made by Razi Falah
+#Copyright (C) 2022 razifalah.com
+#https://razifalah.com
+#According to the applied license: LICENSE GNU General Public License v3.0
+#You do not have the right to republish sell or edit this project, use it only for private use and educational purposes
+
 from typing import Counter
 
 
